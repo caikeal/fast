c1083eae8818df44f53737952d1900ec2777af62
caikeal <546487590@qq.com>
2016-03-24 23:25:02 +0800

    用户帐号解绑

--- app/User.php
+++ app/User.php
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'id_card',
     ];
+
+    public function company(){
+        return $this->belongsTo('App\Company','company_id');
+    }
 }
