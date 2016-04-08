6b129b270bc8de626928ac335eb390f9c7e96c52
caikeal <546487590@qq.com>
2016-04-08 12:49:21 +0800

    针对模版改为||为分割符号

--- app/Http/Controllers/Admin/SalaryController.php
+++ app/Http/Controllers/Admin/SalaryController.php
@@ -192,12 +192,13 @@ class SalaryController extends Controller
         $salaryUpload->upload='app/'.$name;
         $salaryUpload->save();
 
-        $content=$workExcel->get()->toArray();
+        $content=$workExcel->formatDates(true,"Y-m-d")->get()->toArray();
         foreach($content as $k=>$v){
             if(!$v[0]){
                 unset($content[$k]);
             }
         }
+        dd($content);
         if(count($content)<2){
             \Storage::delete($name);
             return response("No Data",404);//必须有实际数据
