7845edcb496bb4470e6a9e18ec1cbe7a284d474c
caikeal <546487590@qq.com>
2016-07-05 20:18:13 +0800

    开启任务软删除

--- app/SalaryTask.php
+++ app/SalaryTask.php
@@ -3,10 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SalaryTask extends Model
 {
+    use SoftDeletes;
     protected $table="salary_task";
+
+    /**
+     * 应该被调整为日期的属性
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     protected $fillable=[
         'manager_id','company_id','receive_id','memo','deal_time','salary_day'
