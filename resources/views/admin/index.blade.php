654dca4fc54321a38166b5e4d77a3b50f9f90b3c
caikeal <546487590@qq.com>
2016-08-09 10:21:27 +0800

    后台页面：普通用户数据统计完成

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,72 +10,60 @@
                     Welcome Back, {{ $manager['name'] }}
                 </div>
             </div>
-            <div class="col-sm-6 text-right text-left-sm p-top-sm">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                        Select Project <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu pull-right" role="menu">
-                        <li><a href="#">Project1</a></li>
-                        <li><a href="#">Project2</a></li>
-                        <li><a href="#">Project3</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Setting</a></li>
-                    </ul>
-                </div>
-
-                <a class="btn btn-default"><i class="fa fa-cog"></i></a>
-            </div>
         </div>
 
         <div class="row m-top-md">
+            @if ($type->contains(1))
             <div class="col-lg-3 col-sm-6">
                 <div class="statistic-box bg-danger m-bottom-md">
                     <div class="statistic-title">
-                        Today Visitors
+                        当月薪资任务
                     </div>
 
                     <div class="statistic-value">
-                        96.7k
+                        {{ $salaryTask }}
                     </div>
 
-                    <div class="m-top-md">11% Higher than last week</div>
+                    <div class="m-top-md">{{ $salaryRate*100 }}% Higher than last month</div>
 
                     <div class="statistic-icon-background">
                         <i class="ion-eye"></i>
                     </div>
                 </div>
             </div>
+            @endif
 
+            @if ($type->contains(2))
             <div class="col-lg-3 col-sm-6">
                 <div class="statistic-box bg-info m-bottom-md">
                     <div class="statistic-title">
-                        Today Sales
+                        当月社保任务
                     </div>
 
                     <div class="statistic-value">
-                        751
+                        {{ $insuranceTask }}
                     </div>
 
-                    <div class="m-top-md">8% Higher than last week</div>
+                    <div class="m-top-md">{{ $insuranceRate*100 }}% Higher than last month</div>
 
                     <div class="statistic-icon-background">
                         <i class="ion-ios7-cart-outline"></i>
                     </div>
                 </div>
             </div>
+            @endif
 
             <div class="col-lg-3 col-sm-6">
                 <div class="statistic-box bg-purple m-bottom-md">
                     <div class="statistic-title">
-                        Today Users
+                        当前答疑解惑数
                     </div>
 
                     <div class="statistic-value">
-                        129
+                        {{ $questionData }}
                     </div>
 
-                    <div class="m-top-md">3% Higher than last week</div>
+                    <div class="m-top-md">&nbsp;</div>
 
                     <div class="statistic-icon-background">
                         <i class="ion-person-add"></i>
@@ -83,23 +71,23 @@
                 </div>
             </div>
 
-            <div class="col-lg-3 col-sm-6">
-                <div class="statistic-box bg-success m-bottom-md">
-                    <div class="statistic-title">
-                        Today Earnings
-                    </div>
+            {{--<div class="col-lg-3 col-sm-6">--}}
+                {{--<div class="statistic-box bg-success m-bottom-md">--}}
+                    {{--<div class="statistic-title">--}}
+                        {{--Today Earnings--}}
+                    {{--</div>--}}
 
-                    <div class="statistic-value">
-                        $124.45k
-                    </div>
+                    {{--<div class="statistic-value">--}}
+                        {{--$124.45k--}}
+                    {{--</div>--}}
 
-                    <div class="m-top-md">7% Higher than last week</div>
+                    {{--<div class="m-top-md">7% Higher than last week</div>--}}
 
-                    <div class="statistic-icon-background">
-                        <i class="ion-stats-bars"></i>
-                    </div>
-                </div>
-            </div>
+                    {{--<div class="statistic-icon-background">--}}
+                        {{--<i class="ion-stats-bars"></i>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
 
     </div><!-- ./padding-md -->
