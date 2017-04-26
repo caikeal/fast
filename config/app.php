90b8d002d1c52f04cd2cc8081a0e384f7c769714
caikeal <546487590@qq.com>
2017-04-26 19:25:30 +0800

    Added 添加错误日志申报功能

--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Sentry\SentryLaravel\SentryLaravelServiceProvider::class,
+
     ],
 
     /*
@@ -205,6 +207,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Excel'     => Maatwebsite\Excel\Facades\Excel::class,
+        'Sentry'    => Sentry\SentryLaravel\SentryFacade::class,
 
     ],
 
